7cb37e53cae61ea59cf029c1a2c7739b76401164
mysta3 <myshawnestallings@gmail.com>
2021-04-09 13:34:48 -0700

    added tabular view of stock list

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -14,7 +14,7 @@ class Stock < ApplicationRecord
       new(ticker: ticker_symbol,
         name: client.company(ticker_symbol).company_name,
         last_price: client.price(ticker_symbol))
-    rescue => exception
+    rescue => e
       return nil
     end 
   end
