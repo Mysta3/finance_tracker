659d76956804f017c8f293b86883b1af2ce37911
mysta3 <myshawnestallings@gmail.com>
2021-04-13 11:20:09 -0700

    implemented removal/addition of friend relationships

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,11 +1,22 @@
 class FriendshipsController < ApplicationController
+
   def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+
+    if current_user.save
+      flash[:notice] = "Following user"
+    else
+      flash[:alert] = "Something went wrong"
+    end
+    
+    redirect_to friends_path
   end
 
   def destroy
     friendship = current_user.friendships.where(friend_id: params[:id]).first
     friendship.destroy
     flash[:notice] = "Stopped Following"
-    redirect_to my_friends_path
+    redirect_to friends_path
   end
 end
